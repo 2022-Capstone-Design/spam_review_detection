e61825a8a95eced14db6c3887dc33c8c3b4f03b7
MoonEeSun <mis2840@gmail.com>
2022-04-29 15:14:06 +0900

    text_preprocessing을 위한 train, test csv 파일

--- Spam_Detection/Text_Preprocessing.py
+++ Spam_Detection/Text_Preprocessing.py
@@ -5,13 +5,21 @@
 
 import re
 import pandas as pd
-# import numpy as np
+import numpy as np
 from konlpy.tag import Okt, Mecab, Hannanum, Kkma, Komoran           # tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer         # embedding
 from sklearn.metrics.pairwise import euclidean_distances            # 유클리디언 거리
 from sklearn.metrics.pairwise import cosine_similarity              # 코사인 유사도
 from gensim.models import FastText                                  # FastText
-from gensim.models import Word2Vec
+from gensim.models import fasttext                                  # FastText pre-trained model
+# from gensim.models import fasttext.util
+from sklearn.ensemble import RandomForestClassifier                 # random forest model import
+from sklearn.svm import SVC                                         # SVM model
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+# from gensim.models import Word2Vec
+# from glove import Corpus, Glove
+# import pickle
 
 
 class Preprocessing:
@@ -22,8 +30,8 @@ class Preprocessing:
     def __init__(self):
         self.id_list = self.Load_ID()                                                                       # 가게 Id list
         self.name_list = self.Load_Name()                                                                   # 가게 이름 list
-        self.one_stop_words = ['은','는','이','가','하','아','것','들','의','있','되','수',
-                            '보','주','등','한','과','랑']                               # 지정 불용어
+        self.one_stop_words = ['은','는','이','가','하','아','것','들','의','있','되','수', '고', '에서',
+                            '보','주','등','한','과','랑', '을', '를', '와', '도', '어요', '다']                   # 지정 불용어
         self.candidate_stop_words = self.Candidate_Stopwords_list_Load()                                    # 한국어불용어100사전
         self.stop_words = self.name_list + list(set(self.one_stop_words + self.candidate_stop_words))       # 가게 이름까지 합친 불용어
         self.tag_flag = 1                                            # [option] tag tokenizer
@@ -64,19 +72,19 @@ class Preprocessing:
             input parameter : None
             output : (list)불용어
         '''
-        file_name = 'Data_Preprocessing/한국어불용어100.txt'
+        file_name = 'Spam_Detection/한국어불용어100.txt'
         korean_stopwords100 = pd.read_csv(file_name, sep = '\t', header = None, names=['word', 'morph', 'rate'])
         return korean_stopwords100['word'].tolist()
 
 
-    def DataLoad(self, id_info):
+    def DataLoad(self):
         '''
         DataLoad() : Insta 게시글 데이터 불러오기
-            input parameter : (int)id_info
+            input parameter : None
             ouput : (df)total_data, (df)text_data
         '''
     
-        csv_name = "Data_Collection/Crawling_Instagram/Insta Post Data/{}_instagram_data.csv".format(id_info)
+        csv_name = "Data_Collection/Crawling_Instagram/Data/instagram_data_AD_label.csv"
         total_data = pd.read_csv(csv_name)
         text_data = total_data.loc[:, ['main_text']]
         
@@ -132,15 +140,13 @@ class Preprocessing:
         else:
             self.tag = Komoran()
 
-
-        test_list = [101]           # test로 id 101 Post data에 대해 수행
-        # for id in self.id_list:
-        for id in test_list:
-            # id에 해당하는 post data 가져오기
-            total_data, text_data = self.DataLoad(id)
-            # cleaning
-            total_data['cleaned_main_text'] = text_data['main_text'].apply(self.cleaning)
-        print(total_data['cleaned_main_text'])
+        # post data 가져오기
+        total_data, text_data = self.DataLoad()
+            
+        # cleaning
+        total_data['cleaned_main_text'] = text_data['main_text'].apply(self.cleaning)
+        print(total_data)
+        print(total_data['cleaned_main_text'][0])
 
         return total_data
     
@@ -161,10 +167,10 @@ class Preprocessing:
         tfidf_metrics = vec_1.fit_transform(cleaned_main_text['cleaned_main_text'])
 
         print("\n---------- Tfid-f ----------")
-        print('광고O, 광고X 사이의 유클리디언 거리 :', euclidean_distances(tfidf_metrics[6], tfidf_metrics[37]))
-        print('광고O, 광고O 사이의 유클리디언 거리 :',euclidean_distances(tfidf_metrics[6], tfidf_metrics[7]))
-        print('광고O, 광고X 사이의 코사인유사도 :',cosine_similarity(tfidf_metrics[6], tfidf_metrics[37]))
-        print('광고O, 광고O 사이의 코사인유사도 :',cosine_similarity(tfidf_metrics[6], tfidf_metrics[7]))
+        print('광고O, 광고X 사이의 유클리디언 거리 :', euclidean_distances(tfidf_metrics[1], tfidf_metrics[3207]))
+        print('광고O, 광고O 사이의 유클리디언 거리 :',euclidean_distances(tfidf_metrics[1], tfidf_metrics[4]))
+        print('광고O, 광고X 사이의 코사인유사도 :',cosine_similarity(tfidf_metrics[1], tfidf_metrics[3207]))
+        print('광고O, 광고O 사이의 코사인유사도 :',cosine_similarity(tfidf_metrics[1], tfidf_metrics[4]))
         print("-----------------------------\n")
         
 
@@ -172,26 +178,32 @@ class Preprocessing:
         vec_2 = FastText(cleaned_main_text['cleaned_main_text'], vector_size=100, window=7,min_count=5, workers=4, sg=1)
     
         print("\n---------- FastText ----------")
-        print('광고O, 광고X 사이의 유사도 :',vec_2.wv.similarity(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][30]))
+        print('광고O, 광고X 사이의 유사도 :',vec_2.wv.similarity(cleaned_main_text['cleaned_main_text'][1], cleaned_main_text['cleaned_main_text'][3207]))
         # print('광고O, 광고X 사이의 코사인유사도 :',vec_2.wv.cosine_similarities(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][30]))
-        print('광고O, 광고O 사이의 유사도 :',vec_2.wv.similarity(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][7]))
+        print('광고O, 광고O 사이의 유사도 :',vec_2.wv.similarity(cleaned_main_text['cleaned_main_text'][1], cleaned_main_text['cleaned_main_text'][4]))
         # print('광고O, 광고O 사이의 코사인유사도 :',vec_2.wv.cosine_similarities(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][7]))
+        print("-----------------------------\n")
+
+        
+        #-- fasttext pre-trained model --#
+        pre_trained_model = fasttext.load_facebook_model('cc.ko.300.bin')
+        print("\n---------- pre-trained FastText ----------")
+        print('광고O, 광고X 사이의 유사도 :', pre_trained_model.similarity(cleaned_main_text['cleaned_main_text'][1], cleaned_main_text['cleaned_main_text'][3207]))
+        print('광고O, 광고O 사이의 유사도 :',pre_trained_model.similarity(cleaned_main_text['cleaned_main_text'][1], cleaned_main_text['cleaned_main_text'][4]))
         print("-----------------------------\n")
         
 
-        '''
-        #-- Word2Vec --#
-        vec_3 = Word2Vec(cleaned_main_text['cleaned_main_text'], vector_size=100, window=7,min_count=5, workers=4, sg=1)
-
-        print("\n---------- Word2Vec ----------")
-        print('광고O, 광고X 사이의 유사도 :',vec_3.wv.similarity(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][30]))
-        # print('광고O, 광고X 사이의 코사인유사도 :',vec_3.wv.cosine_similarities(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][30]))
-        print('광고O, 광고O 사이의 유사도 :',vec_3.wv.similarity(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][7]))
-        # print('광고O, 광고O 사이의 코사인유사도 :',vec_3.wv.cosine_similarities(cleaned_main_text['cleaned_main_text'][6], cleaned_main_text['cleaned_main_text'][7]))
-        print("-----------------------------\n")
-        '''
-
         return total_data
+    
+    '''
+    def classification_model(self):
+        rf = RandomForestClassifier(max_depth=2, random_state=0)
+        rf.fit(X, y)
+
+
+        svm = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+        svm.fit(X, y)
+    '''
         
 
 
